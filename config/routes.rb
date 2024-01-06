4abb5b356229783cf84e749799dc73a77bafd07d
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-01-07 02:45:44 +0530

    - Implemented the show method in the ArticlesController to fetch an article by ID. - Added the show.html.erb view for displaying a single article.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "articles#index"
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "posts#index"
 end
