07a69cdd090dfa5b0a0602dd9b473a6d20933c2f
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-01-07 12:14:49 +0530

    Update route to resource :articles, add new action, and implement article creation
    - Updated the route in config/routes.rb to use the `resource` shorthand for articles.
    - Added a new action in the ArticlesController to handle article creation.
    - Implemented the new.html.erb view to facilitate article creation.
    - Added validation for input in the article creation process.
    - Included a link on the root page to navigate to the article creation page.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
   # Defines the root path route ("/")
   # root "posts#index"
 end
