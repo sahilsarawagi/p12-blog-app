8c02396c124af544c903e0a6c817604f75cab574
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-01-08 20:39:47 +0530

    Add comments functionality
    - Updated routes.rb to include resources :comments.
    - Modified show.html.erb to allow adding comments and display previous comments.
    - Added the create action in CommentsController to handle comment creation.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   root "articles#index"
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
-  resources :articles
+  resources :articles do 
+    resources :comments
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
